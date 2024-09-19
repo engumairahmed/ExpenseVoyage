7885ce1628854444144f0cb3ef1afa86996d091b
RayyanAhmed009 <158506139+RayyanAhmed009@users.noreply.github.com>
2024-09-19 23:03:36 +0500

    User UI updated
    User UI updated

---
+++ ExpenseVoyage/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseVoyage.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
