983d929bd8893a698d5425a3e448f65748f6677f
AimanNaseem <111281689+AimanNaseem@users.noreply.github.com>
2024-09-19 17:09:51 +0500

    dashboard
    index,layout

---
+++ ExpenseVoyage/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseVoyage.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
